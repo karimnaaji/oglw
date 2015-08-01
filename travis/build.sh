85afc0f234bd056b176acd92c6eed8a87c439ff4
Karim Naaji <karim.naaji@gmail.com>
2015-08-01 14:49:33 -0400

    travis

--- travis/build.sh
+++ travis/build.sh
@@ -4,14 +4,12 @@ set -e
 set -o pipefail
 
 if [[ ${PLATFORM} == "osx" ]]; then
-    echo "Building osx project"
     mkdir build && cd build
     cmake ..
     make
 fi
 
 if [[ ${PLATFORM} == "linux" ]]; then
-    echo "Building linux project"
     mkdir build && cd build
     cmake ..
     make
