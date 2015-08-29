34040578e5062d6bc7a379ce1a3d9a0c2d294cc6
Karim Naaji <karim.naaji@gmail.com>
2015-08-29 16:09:29 -0400

    Clean geometry shader

--- samples/tile/resources/default.glsl
+++ samples/tile/resources/default.glsl
@@ -5,55 +5,44 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 
 uniform mat4 mvp;
-uniform mat4 mv;
 
-out vec3 incident;
-out vec3 n;
+out vec3 vNormal;
 
 void main() {
-    vec4 eyep = vec4(position, 1.0);
-    incident = normalize(vec3(mv * eyep));
-    n = normal;
-
+    vNormal = normal;
     gl_Position = mvp * vec4(position, 1.0);
 }
 
 #pragma end:vertex
-#pragma begin:fragment
-#version 330
-
-uniform sampler2D tex;
-uniform mat3 normalmat;
-
-out vec4 outColour;
-
-in vec3 incident;
-in vec3 n;
-
-void main(void) {
-    vec3 r = reflect(incident, normalize(normalmat * n));
-
-    float m = 2.0 * sqrt(
-        pow(r.x, 2.0) +
-        pow(r.y, 2.0)  +
-        pow(r.z + 1.0, 2.0)
-    );
-    vec2 uv = r.xy / m + 0.5;
-
-    outColour = vec4(texture(tex, uv).rgb, 1.0);
-}
-
-#pragma end:fragment
 #pragma begin:geom
 #version 330
 
-layout(points) in;
-layout(points, max_vertices = 1) out;
+layout(line_strip) in;
+layout(line_strip, max_vertices = 2) out;
+
+in vec3 vNormal[];
+
+out vec3 fNormal;
 
 void main() {
     gl_Position = gl_in[0].gl_Position;
     EmitVertex();
     EndPrimitive();
+    fNormal = vNormal[0];
 }
 
 #pragma end:geom
+#pragma begin:fragment
+#version 330
+
+uniform mat3 normalmat;
+
+out vec4 outColour;
+
+in vec3 fNormal;
+
+void main(void) {
+    outColour = vec4(normalmat * fNormal, 1.0);
+}
+
+#pragma end:fragment
