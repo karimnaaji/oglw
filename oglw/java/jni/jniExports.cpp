c2dac0e2eafb50e3d43cd46c539da130d1a57e06
Karim Naaji <karim.naaji@gmail.com>
2015-08-29 17:58:44 -0400

    Addd jni Exports

---
+++ oglw/java/jni/jniExports.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include "core/log.h"
+
+extern "C" {
+
+    JNIEXPORT void JNICALL Java_OGLWApp_helloOGLW(JNIEnv* jniEnv, jobject obj) {
+        INFO("Hello from OGLW");
+    }
+
+}
