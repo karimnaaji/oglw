1b3745b0c6ada74c56f27362fbc43bbbd4245104
Karim Naaji <karim.naaji@gmail.com>
2015-09-09 18:07:12 -0400

    Remove geometry shader from tile example

--- samples/tile/resources/default.glsl
+++ samples/tile/resources/default.glsl
@@ -6,32 +6,15 @@ layout (location = 1) in vec3 normal;
 
 uniform mat4 mvp;
 
-out vec3 vNormal;
+out vec3 fNormal;
 
 void main() {
-    vNormal = normal;
+    fNormal = normal;
     gl_Position = mvp * vec4(position, 1.0);
 }
 
 #pragma end:vertex
-#pragma begin:geom
-#version 330
 
-layout(line_strip) in;
-layout(line_strip, max_vertices = 2) out;
-
-in vec3 vNormal[];
-
-out vec3 fNormal;
-
-void main() {
-    gl_Position = gl_in[0].gl_Position;
-    EmitVertex();
-    EndPrimitive();
-    fNormal = vNormal[0];
-}
-
-#pragma end:geom
 #pragma begin:fragment
 #version 330
 
