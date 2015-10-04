7340b175a4b0d452f8b65265c88cede22343e9ed
Karim Naaji <karim.naaji@gmail.com>
2015-10-04 15:26:17 -0400

    wip new sample

--- samples/render-target/main.cpp
+++ samples/render-target/main.cpp
@@ -17,21 +17,45 @@ class TestApp : public OGLW::App {
         void init() override;
 
     private:
-        uptr<OGLW::RenderTarget> m_renderTarget;
+        uptr<Mesh<glm::vec4>> m_plane;
+        uptr<Mesh<glm::vec4>> m_quad;
+        std::vector<uptr<RawMesh>> m_cubes;
+        uptr<Shader> m_shader;
 };
 OGLWMain(TestApp);
 
 void TestApp::init() {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     m_camera.setPosition({0.0, -0.5, 14.0});
-    m_renderTarget = std::make_unique<RenderTarget>();
-    m_renderTarget->create(800, 600);
+    m_plane = plane(30.f, 30.f, 1, 1);
+    for (int i = 0; i < 64; ++i) {
+        m_cubes.push_back(std::move(cube(0.3)));
+    }
+    m_shader = uptr<Shader>(new Shader("default.glsl"));
 }
 
 void TestApp::update(float _dt) {
-
+    updateFreeFlyCamera(_dt, 'S', 'W', 'A', 'D', 1e-3f);
 }
 
 void TestApp::render(float _dt) {
-    m_renderTarget->apply(800, 600);
+    glm::mat4 model, mvp, view, proj;
+
+    model = glm::mat4();
+    view = m_camera.getViewMatrix();
+    proj = m_camera.getProjectionMatrix();
+
+    int i = 0;
+    for (int x = 0; x < 4; x++) {
+        for (int y = 0; y < 4; y++) {
+            for (int z = 0; z < 4; z++) {
+                const auto& cube = m_cubes[i++];
+                model = glm::translate(glm::mat4(), glm::vec3(x, y, z));
+                mvp = proj * view * model;
+                m_shader->setUniform("mvp", mvp);
+                cube->draw(*m_shader);
+            }
+        }
+    }
 }
 
