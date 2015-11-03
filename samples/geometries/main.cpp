6178009b9a50db8243c0fa741373e3f2fe94625a
Karim Naaji <karim.naaji@gmail.com>
2015-11-02 19:21:56 -0500

    Cleanup

--- samples/geometries/main.cpp
+++ samples/geometries/main.cpp
@@ -6,24 +6,6 @@
 template <class T>
 using uptr = std::unique_ptr<T>;
 using namespace OGLW;
-using namespace gam;
-
-// ------------------------------------------------------------------------------
-// Gamma sound processing configuration
-Sine<> src(220);
-Pan<> pan;
-LFO<> panMod(0.5);
-float s;
-
-void audioCB(AudioIOData& io) {
-    while(io()) {
-        s = src() * 0.2;
-        pan.pos(panMod.tri());
-        float2 xy = pan(s);
-        io.out(0) = xy.x;
-        io.out(1) = xy.y;
-    }
-}
 
 // ------------------------------------------------------------------------------
 // OGLW App
@@ -38,14 +20,13 @@ class TestApp : public App {
         uptr<Shader> m_shader;
         uptr<Mesh<glm::vec4>> m_geometry;
         uptr<OGLW::Texture> m_texture;
-
-        float m_xrot = 0.f, m_yrot = 0.f;
 };
-OGLWMainGamma(TestApp, audioCB);
+OGLWMain(TestApp);
 
 void TestApp::init() {
-    glClearColor(0.1, 0.1, 0.25, 1.0);
-    m_camera.setPosition({0.0, -1.0, 18.0});
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    m_camera.setPosition({0.0, 0.0, 5.0});
+    m_camera.setFov(45.f);
 
     m_shader = uptr<Shader>(new Shader("default.glsl"));
 
@@ -57,28 +38,20 @@ void TestApp::init() {
 
     m_texture = uptr<OGLW::Texture>(new OGLW::Texture("perlin.png", options));
 
-    m_geometry = plane(20.f, 20.f, 150, 150);
+    m_geometry = plane(40.f, 40.f, 350, 350);
 }
 
 void TestApp::update(float _dt) {
-    m_xrot += m_cursorX;
-    m_yrot += m_cursorY;
+    updateFreeFlyCamera(_dt, 'S', 'W', 'A', 'D', 1e-3f, 20.f);
 }
 
 void TestApp::render(float _dt) {
-    glm::mat4 model;
-    glm::mat4 view = m_camera.getViewMatrix();
-
-    model = glm::rotate(model, m_xrot * 1e-2f, glm::vec3(0.0, 1.0, 0.0));
-    model = glm::rotate(model, m_yrot * 1e-2f, glm::vec3(1.0, 0.0, 0.0));
-
-    glm::mat4 mvp = m_camera.getProjectionMatrix() * view * model;
+    glm::mat4 mvp = m_camera.getProjectionMatrix() * m_camera.getViewMatrix();
 
     m_texture->bind(0);
 
-    m_shader->setUniform("mvp", mvp);    
+    m_shader->setUniform("mvp", mvp);
     m_shader->setUniform("tex", 0);
-    m_shader->setUniform("t0", s);
     m_shader->setUniform("time", m_globalTime);
 
     RenderState::depthTest(GL_TRUE);
