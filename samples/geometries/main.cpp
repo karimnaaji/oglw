5b9b868a58aa24bebf7abac7ca7337ff4456e5ee
Karim Naaji <karim.naaji@gmail.com>
2015-11-07 00:12:58 -0500

    WIP reflections

--- samples/geometries/main.cpp
+++ samples/geometries/main.cpp
@@ -21,18 +21,24 @@ class TestApp : public App {
         uptr<Shader> m_waterShader;
         uptr<Mesh<glm::vec4>> m_geometry;
         uptr<Mesh<glm::vec4>> m_waterGeometry;
-        uptr<OGLW::Texture> m_texture;
+        uptr<Texture> m_texture;
+        uptr<RenderTarget> m_renderTarget;
+        uptr<Camera> m_reflectionCamera;
 };
 OGLWMain(TestApp);
 
 void TestApp::init() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
 
     // default camera
     m_camera.setPosition({0.0, -3.0, 5.0});
     m_camera.setFar(200.f);
     m_camera.setNear(0.1f);
     m_camera.setFov(30);
+
+    m_reflectionCamera = uptr<Camera>(new Camera());
+    m_reflectionCamera->setFar(200.0f);
+    m_reflectionCamera->setNear(0.1f);
+    m_reflectionCamera->setFov(30);
 
     m_shader = uptr<Shader>(new Shader("default.glsl"));
     m_waterShader = uptr<Shader>(new Shader("water.glsl"));
@@ -47,6 +53,12 @@ void TestApp::init() {
 
     m_geometry = plane(20.f, 20.f, 350, 350);
     m_waterGeometry = plane(20.f, 20.f, 250, 250);
+
+    RenderTargetSetup setup;
+    setup.useDepth = true;
+    m_renderTarget = std::make_unique<OGLW::RenderTarget>(setup);
+    m_renderTarget->create(1024, 720);
+
 }
 
 void TestApp::update(float _dt) {
@@ -54,6 +66,7 @@ void TestApp::update(float _dt) {
 }
 
 void TestApp::render(float _dt) {
+    float yWaterPlane = 1.0f;
     glm::mat4 model = glm::rotate(glm::mat4(), (float) M_PI_2, glm::vec3(1.0, 0.0, 0.0));
     glm::mat4 mvp = m_camera.getProjectionMatrix() * m_camera.getViewMatrix() * model;
 
@@ -72,12 +85,18 @@ void TestApp::render(float _dt) {
     RenderState::cullFace(GL_BACK);
     RenderState::blending(GL_FALSE);
 
+    //m_renderTarget->apply(1024, 720, 0xffffffff);
+
     m_geometry->draw(*m_shader);
+
+    //RenderTarget::applyDefault(1024, 720, 0xffffffff);
+
     mvp = glm::translate(mvp, glm::vec3(0.0, 0.0, 0.8));
 
     m_waterShader->setUniform("mvp", mvp);
     m_waterShader->setUniform("time", m_globalTime);
     m_waterShader->setUniform("modelView", m_camera.getViewMatrix() * model);
+    m_waterShader->setUniform("yWaterPlane", yWaterPlane);
     m_waterShader->setUniform("normalMatrix", glm::inverse(glm::transpose(glm::mat3(mvp))));
 
     RenderState::blending(GL_TRUE);
