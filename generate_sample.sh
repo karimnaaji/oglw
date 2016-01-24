defd78bfd5b7c368b4a59aee98c21a86896aedc3
Karim Naaji <karim.naaji@gmail.com>
2016-01-23 21:09:19 -0500

    Add script to generate samples

---
+++ generate_sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Sample name > "
+read sample_name
+echo "Sample name: $sample_name"
+mkdir samples/$sample_name
+mkdir samples/$sample_name/resources
+sed 's/$project_name/'$sample_name'/g' template/main.txt > samples/$sample_name/main.cpp
+sed 's/$project_name/'$sample_name'/g' template/cmake.txt > samples/$sample_name/CMakeLists.txt
+
